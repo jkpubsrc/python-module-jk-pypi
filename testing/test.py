ba0d5758ebdba4c1268a228755b26f63017c88a0
jkpubsrc <pubsrc@binary-overflow.de>
2019-09-12 23:19:53 +0200

    First release.

---
+++ testing/test.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+
+import jk_pypi
+
+
+
+
+
+pypi = jk_pypi.PyPi()
+
+print(pypi.getModuleVersion("jk-simpleexec"))
+
+print(pypi.getModuleVersion("jk-simpleexecAKJHKEJH"))
+
+
+
+
+
+
